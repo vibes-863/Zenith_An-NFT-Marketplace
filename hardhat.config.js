edad3414e0cdfd4fa7eb29bbafe07648d3cd3ce8
shyamkrishna <ashyamkrishna33@gmail.com>
2023-06-03 14:47:32 +0800

    Setup Smart contract for the MarketPlace

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [privateKey]
+    },
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [privateKey]
+    }
+    
+  },
+
   solidity: "0.8.18",
 };
