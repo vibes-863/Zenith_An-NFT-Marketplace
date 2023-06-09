3ea08d282cd1f07dedcc974bfe570cd3057c4751
shyamkrishna <ashyamkrishna33@gmail.com>
2023-06-09 14:52:35 +0800

    Setup React Home Component to interact with the smart contracts

--- app/page.js
+++ app/page.js
@@ -1,44 +1,114 @@
 import './globals.css'
 import Link from 'next/link'
 
-function Marketplace({ Component, pageProps }) {
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import {
+  nftaddress, nftmarketaddress
+} from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTmarket.json'
+
+let rpcEndpoint = null
+
+if (process.env.NEXT_PUBLIC_WORKSPACE_URL) {
+  rpcEndpoint = process.env.NEXT_PUBLIC_WORKSPACE_URL
+}
+
+export default function Home() {
+
+  /**The 'Home' component maintains state using the 'nfts' and 'loadingState' variables.
+   * The 'nfts' state holds an array of NFT objects, while 'loadingState' tracks the
+   *  loading status of the NFTs. */
+  const [nfts, setNfts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+
+  //The 'useEffect' hook is used to load the NFTs when the component mounts. 
+  useEffect(() => {
+    loadNFTs()
+  }, [])
+
+  /**The loadNFTs function is an asynchronous function responsible for fetching and
+   *  processing NFT data from the marketplace.*/
+  async function loadNFTs() {  
+    
+    //It creates an instance of JsonRpcProvider from ethers library using the rpcEndpoint.
+    const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint)
+
+    //It creates instances of the NFT and marketplace contracts using their addresses and ABIs.
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+
+    //It calls the fetchMarketItems function on the marketplace contract to retrieve the market items.
+    const data = await marketContract.fetchMarketItems()
+    
+    /**It then maps over the retrieved data to create an array of item objects
+     * by fetching additional metadata for each item using the NFT contract. */
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        itemId: i.itemId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item
+    }))
+    /**The resulting array of items is stored in the nfts state variable, and the loadingState is set to 'loaded'. */
+    setNfts(items)
+    setLoadingState('loaded') 
+  }
+
+  /**The buyNft function is an asynchronous function responsible for buying an NFT from the marketplace.*/
+  async function buyNft(nft) {
+
+    //It creates an instance of Web3Modal and prompts the user to connect to their Ethereum wallet.
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+
+    //It then creates a Web3Provider using the established connection and obtains the signer (account) for executing transactions.
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    //An instance of the marketplace contract is created using the nftmarketaddress, ABI, and signer.
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+
+    //The price of the NFT is parsed from a string to a BigNumber using ethers.utils.parseUnits.
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
+    
+    //The createMarketSale function is called on the contract to execute the transaction and buy the NFT.
+    const transaction = await contract.createMarketSale(nftaddress, nft.itemId, {
+      value: price
+    })
+
+    // After the transaction is successfully completed, the loadNFTs function is called to refresh the list of NFTs.
+    await transaction.wait()
+    loadNFTs()
+  }
+
+  //If the loadingState is 'loaded' and there are no items in the nfts array...
+  if (loadingState === 'loaded' && !nfts.length) return (<h1>No items in marketplace</h1>)
+
   return (
-    <><head>
-      <link
+    <>
+    
+          <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossOrigin="anonymous"
         />
-    </head><body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand nav-txt" href="#">Zenith</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <Link class="nav-link nav-txt" href='/'>Home</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link nav-txt" href='/create-nft'>Sell Digital Assets</Link>
-              </li>
-              
-              <li class="nav-item">
-                <Link class="nav-link nav-txt" href='/my-asset'>My Digital Assets</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link nav-txt" href='/dashboard'>Dashboard</Link>
-              </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Connect Wallet</button>
-            </form>
-          </div>
-        </nav>
+    
+        
         <div id="content">
           <div class="top">
             <div class="main-txt">
@@ -63,9 +133,7 @@ function Marketplace({ Component, pageProps }) {
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossOrigin="anonymous"
       />
-      </body></>
+     </>
                 
   )
 }
-
-export default Marketplace
