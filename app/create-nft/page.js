88d09e10943e2927191e1f0bceca720a56ea673a
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2023-06-15 17:36:00 +0530

    Javascript still doesn't work

--- app/create-nft/page.js
+++ app/create-nft/page.js
@@ -124,9 +124,9 @@ export default function CreateNFT() {
                 />
             </div>
 
-            <div class="mb-3">
+            <div className="mb-3">
                 <textarea
-                    class="form-control"
+                    className="form-control"
                     id="exampleFormControlTextarea1"
                     rows="3"
                     placeholder="Asset Description"
@@ -147,8 +147,8 @@ export default function CreateNFT() {
                 />
             </div>
 
-            <div class="mb-3">
-                <input class="form-control" type="file" id="formFile" onChange={onChange}/>
+            <div className="mb-3">
+                <input className="form-control" type="file" id="formFile" onChange={onChange}/>
             </div>
 
             {
@@ -157,7 +157,7 @@ export default function CreateNFT() {
                 )
             }
             
-            <button type="button" class="btn btn-dark" onClick={createNFT}>
+            <button type="button" className="btn btn-dark" onClick={createNFT}>
             Create Digital Asset
             </button>
         </div>
--- app/page.js
+++ app/page.js
@@ -122,13 +122,6 @@ export default function Home() {
   
   return (
     <>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossOrigin="anonymous"
-      ></link>
-
       <section id="heroe">
         <div id="content">
           <div className="top">
@@ -161,12 +154,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-      
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        crossOrigin="anonymous"
-      ></script>
     </>
   );
 }
