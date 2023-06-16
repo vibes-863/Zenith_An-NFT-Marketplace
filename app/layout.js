53cfec2dfd4a36b768d87ed01ac60452e8cdd644
shyamkrishna <ashyamkrishna33@gmail.com>
2023-06-16 18:08:15 +0800

    Fix Javascript

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./globals.css";
 import Link from "next/link";
 import Script from "next/script";
@@ -13,56 +13,31 @@ function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>Zenith</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
+
       </head>
 
       <body>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand nav-txt" href="#">
-            Zenith
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <Link className="nav-link nav-txt" href="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link nav-txt" href="/create-nft">
-                  Sell Digital Assets
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link nav-txt" href="/my-assets">
-                  My Digital Assets
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link nav-txt" href="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Zenith</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="/create-nft">Sell Digital Assets</a>
+                <a class="nav-link" href="/my-assets">My Digital Assets</a>
+                <a class="nav-link" href="/dashboard">Dashboard</a>
+              </div>
+            </div>
           </div>
         </nav>
+
         <main>{children}</main>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-          strategy="beforeInteractive"
-        />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
       </body>
     </html>
   );
