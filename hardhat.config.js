49a923e03f64026a9a4eef2184bf07b2e6750472
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2023-06-19 18:07:27 +0530

    setup polygon network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,26 @@
 require("@nomiclabs/hardhat-waffle");
-
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString
+const apiKey = "1c04d8dd403749a4b007a8fef3e15142";
 
 module.exports = {
- /* defaultNetwork: "hardhat",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
-    
+
     mumbai: {
       // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
+      url: `https://polygon-mumbai.infura.io/v3/${apiKey}`,
+      accounts: [privateKey],
     },
     matic: {
       // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [privateKey]
-    }
-    
-  },*/
+      url: `https://polygon-mainnet.infura.io/v3/${apiKey}`,
+      accounts: [privateKey],
+    },
+  },
 
   solidity: "0.8.18",
 };
