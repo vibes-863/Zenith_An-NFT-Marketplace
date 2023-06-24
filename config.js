8acd578584671e9f517f5d71cb5129e08de0d146
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2023-06-24 17:02:14 +0530

    added configuration for mumbai testnet

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// The below is for Mumbai Testnet
+export const nftaddress = "0x47Df2C2f021d8233F4d31282E10C96BF0F3de0cc";
+export const nftmarketaddress = "0xFe1227314d4403265d869B05c087159ded7a10C9";
+
+// The below is for local network
+//export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+//export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
