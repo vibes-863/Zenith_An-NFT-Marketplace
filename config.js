14e8a48257ae16cd95e26a24793da0c63b300978
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2023-07-21 13:34:04 +0530

    re-deployed smart contracts

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // The below is for Mumbai Testnet
-export const nftaddress = "0xeDeEB4a1A1D578F2d0Fc329ad22976378e40825A";
-export const nftmarketaddress = "0xd03C5CE5286Eb538F641038564BC39c4d0501Afe";
+export const nftaddress = "0xAF9721Baf4EB4e2A68002105b2939eEa79033aAF";
+export const nftmarketaddress = "0x46F24a26dC93feA9f513bBe804C07Ca104E75549";
 
 // The below is for local network
 //export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
