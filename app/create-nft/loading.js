e71965911cc3d2f21420d73944d2f70683216fb1
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2023-07-22 23:53:57 +0530

    added loading page for all pages

---
+++ app/create-nft/loading.js
@@ -0,0 +1,10 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+    return (
+      <div className="loadingDiv">
+        <span class="loader">
+          <span class="loader-inner"></span>
+        </span>
+      </div>
+    );
+}
