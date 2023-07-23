6fa162fa1c69dc5d43a34b625496d3ed2cf5d95c
Vaibhav Dileep Pillai <vaibhavdpillai863@outlook.com>
2023-07-23 22:39:16 +0530

    redeployed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // The below is for Mumbai Testnet
-export const nftaddress = "0xfb03Fdca113D811c8f72E76EDAc6AFb00D72bdd5";
-export const nftmarketaddress = "0xdBf83a82760cc17d975dBb57b97084a73102B49E";
+export const nftaddress = "0x561C504C137DF0a3754ba0d908e6f11CEC504B77";
+export const nftmarketaddress = "0xA4928b63102394484Ba9510493993C8E426FF1D9";
 
 // The below is for local network
 //export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
